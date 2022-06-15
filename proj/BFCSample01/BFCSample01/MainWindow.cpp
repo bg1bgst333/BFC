36f1488b0f9b629eed872fba4ad9cf715e4f050a
miura <miura@divakk.co.jp>
2022-06-15 16:57:14 +0900

    -サンプルプロジェクトBFCSample01を追加.

---
+++ proj/BFCSample01/BFCSample01/MainWindow.cpp
@@ -0,0 +1,77 @@
+// ヘッダのインクルード
+// 独自のヘッダ
+#include "MainWindow.h"	// CMainWindow
+
+// ウィンドウクラス登録関数RegisterClass.
+BOOL CMainWindow::RegisterClass(HINSTANCE hInstance) {
+
+	// ウィンドウクラス名は"CMainWindow".
+	return CWindow::RegisterClass(hInstance, _T("CMainWindow"));	// CWindow::RegisterClassでウィンドウクラス名"CMainWindow"を登録.
+
+}
+
+// コンストラクタCMainWindow()
+CMainWindow::CMainWindow() {
+
+}
+
+// デストラクタ~CMainWindow()
+CMainWindow::~CMainWindow() {
+
+	// メンバの終了処理.
+	Destroy();	// Destroyで子ウィンドウの破棄.
+
+}
+
+// ウィンドウ作成関数Create.(ウィンドウクラス名省略バージョン.)
+BOOL CMainWindow::Create(LPCTSTR lpctszWindowName, DWORD dwStyle, int x, int y, int iWidth, int iHeight, HWND hWndParent, HMENU hMenu, HINSTANCE hInstance) {
+
+	// ウィンドウクラス名は"CMainWindow".
+	return CWindow::Create(_T("CMainWindow"), lpctszWindowName, dwStyle, x, y, iWidth, iHeight, hWndParent, hMenu, hInstance);	// CWindow::Createにウィンドウクラス名"CMainWindow"を指定.
+
+}
+
+// ウィンドウ破棄関数Destroy
+BOOL CMainWindow::Destroy() {
+
+	// 変数の初期化.
+	BOOL bRet = FALSE;	// bRetをFALSEで初期化.
+
+	// 自身のウィンドウも破棄.(OnCloseの前にOnDestroyが呼ばれるのはおかしいのでコメントアウト.)
+	//return CWindow::Destroy();	// CWindow::Destroyを呼ぶ.
+	return bRet;	// bRetを返す.
+
+}
+
+// ウィンドウの作成が開始された時.
+int CMainWindow::OnCreate(HWND hwnd, LPCREATESTRUCT lpCreateStruct) {
+
+	// 親クラスのOnCreateを呼ぶ.
+	return CWindow::OnCreate(hwnd, lpCreateStruct);	// CWindow::OnCreateを呼び, 戻り値を返す.
+
+}
+
+// ウィンドウが破棄された時.
+void CMainWindow::OnDestroy() {
+
+	// CWindowのOnDestroyを呼ぶ.
+	CWindow::OnDestroy();	// CWindow::OnDestroyを呼ぶ.
+
+}
+
+// ウィンドウが閉じられる時.
+int CMainWindow::OnClose() {
+
+	// メッセージボックスで"Close CMainWindow OK?"と表示.
+	int iRet = MessageBox(m_hWnd, _T("Close CMainWindow OK?"), _T("BFCSample01"), MB_OKCANCEL);	// MessageBoxで"Close CMainWindow OK?"と表示し, 戻り値をiRetに格納.
+	if (iRet != IDOK) {	// OK以外.(Cancelなど.)
+		return -1;	// -1を返す.
+	}
+
+	// このウィンドウの破棄.
+	Destroy();	// Destroyでこのウィンドウの破棄処理.
+
+	// OKなので閉じる.
+	return CWindow::OnClose();	// 親クラスのOnCloseを呼ぶ.(親クラスのOnCloseは常に閉じる処理になっている.)
+
+}
