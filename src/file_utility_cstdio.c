d0a6885d208eec2c163fc1e022cfc5c19947bc36
bg1bgst333 <bg1bgst333@gmail.com>
2024-02-15 15:55:31 +0900

    -モジュール更新, サンプル更新  -file_utility_cstdio.h, file_utility_cstdio.c更新.  -サンプルBFCSample16更新.

--- src/file_utility_cstdio.c
+++ src/file_utility_cstdio.c
@@ -204,3 +204,30 @@ int write_file_binary_cstdio(const char* path, const void* bytes, size_t size) {
 	return -1;	/* returnで-1を返す. */
 
 }
+
+/* C標準入出力ライブラリ関数でバイナリファイル入力. */
+size_t read_file_binary_cstdio(const char* path, void* bytes, size_t size) {
+
+	/* 構造体・ポインタの宣言・初期化. */
+	FILE* fp = NULL;	/* ファイルポインタfpをNULLで初期化. */
+	size_t ret;	/* 読み込んだ文字数ret. */
+
+	/* ファイルを開く. */
+	fp = fopen(path, "rb");	/* fopenでバイナリ読み込みで開く. */
+	if (fp != NULL) {	/* fpがNULLでない時. */
+
+		/* ファイルからバイナリを読み込む. */
+		ret = fread(bytes, sizeof(char), size, fp);	/* freadでfpから入力しbytesに格納, 戻り値はretに格納. */
+
+		/* fpを閉じる. */
+		fclose(fp);	/* fcloseでfpを閉じる. */
+
+		/* retを返す. */
+		return ret;	/* returnでretを返す. */
+
+	}
+
+	/* ファイルを開けない場合は, 0を返す. */
+	return 0;	/* returnで0を返す. */
+
+}
